09c2015d5236aec19596cc8a50aa69a191f77870
zManuu <60520810+zManuu@users.noreply.github.com>
2023-02-03 19:09:35 +0100

    Removed generator.ts (unused)

--- src/utils/generator.ts
+++
@@ -1,7 +0,0 @@
-export function generateItems<T>(count: number, fn: (i: number) => T): T[] {
-    const res: T[] = []
-    for (let i=0; i<count; i++) {
-        res.push(fn(i))
-    }
-    return res
-}
